f8125e9d383a5d946413f03d24868606e8399afa
Simon Opitz <simon@iss-opitz.de>
2015-02-07 09:27:35 +0100

    add bg images

--- website/js/zerif.js
+++ website/js/zerif.js
@@ -4,9 +4,10 @@
 $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
-    { src:'images/backgrounds/bg1.jpg', fade:1000 },
-    { src:'images/backgrounds/bg2.jpg', fade:1000 },
-    { src:'images/backgrounds/bg3.jpg', fade:1000 }
+    { src:'images/backgrounds/background1.jpg', fade:2000 },
+    { src:'images/backgrounds/background2.jpg', fade:2000 },
+    { src:'images/backgrounds/background3.jpg', fade:2000 },
+    { src:'images/backgrounds/background4.jpg', fade:2000 }
   ]
 });
       
