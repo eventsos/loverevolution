c95b5b8828ca983e6e640cc512a04c3e881b54f3
Simon Opitz <simon@iss-opitz.de>
2015-03-25 20:54:49 +0100

    fix stay in touch

--- sources/addMailinglist.php
+++ sources/addMailinglist.php
@@ -8,11 +8,10 @@ $message = "Bitte '$email' mit in den E-Mailverteiler aufnehmen.";
 $to      = 'simon@opitzfamily.de';
 
 $headers = 'From: '.$email. "\r\n" .
-    'Reply-To: '. $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 if ($email != "" && $message != "") {
-    mail($to, $subject, $email, $headers);
+    mail($to, $subject, $message, $headers);
     echo "success";
 } else {
     echo "missingFields";
