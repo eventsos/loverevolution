f984738a16e5bfc75bc232a75d1f75ac05f7f8e9
Simon Opitz <simon@iss-opitz.de>
2015-03-25 20:44:50 +0100

    stay in touch mail

---
+++ sources/addMailinglist.php
@@ -0,0 +1,19 @@
+<?php
+
+$email = $_POST['email'];
+$subject = "Newsletter";
+$message = "Bitte '$email' mit in den E-Mailverteiler aufnehmen.";
+
+
+$to      = 'simon@opitzfamily.de';
+
+$headers = 'From: '.$email. "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if ($email != "" && $message != "") {
+    mail($to, $subject, $email, $headers);
+    echo "success";
+} else {
+    echo "missingFields";
+}
